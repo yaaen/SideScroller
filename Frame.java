c16a43b283cda129118bc9abf925b4feefb5c57a
Derek <derek.schuster@yahoo.com>
2015-03-02 17:24:57 -0500

    updated Frame.java

--- Frame.java
+++ Frame.java
@@ -1,3 +1,8 @@
 public class Frame extends JFrame{
-  
+  public Frame(){
+    setSize(1000, 1000);
+    setTitle("Side Scroller");
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 }
