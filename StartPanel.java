ea54037baa4890f92918fbeeca9260c2466bd681
Derek <derek.schuster@yahoo.com>
2015-03-02 18:48:07 -0500

    updated StartPanel.java

--- StartPanel.java
+++ StartPanel.java
@@ -1,10 +1,50 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class StartPanel extends JPanel{
   public StartPanel(){
-    
+     setSize(1600, 1000);
+        setLayout(null);
+        
+        int count = 1;
+        int yValue = 150;
+        for(int r = 0; r < 3; r++){
+            for(int c = 0; c < 8; c++){
+                levels[r][c] = new JButton(String.valueOf(count));
+                levels[r][c].setLocation(c*190 + 80, yValue);
+                levels[r][c].setSize(100, 100);
+                this.add(levels[r][c]);
+                count++;
+            }
+            yValue += 200;
+        }
   }
   
   @Override
   public void paint(Graphics g){
-    
+      //TODO:
+      //paint the background of the start screen
   }
+  
+  public class event implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int rr = 0, cc = 0;
+            for(int r = 0; r < 3; r++){
+                for(int c = 0; c < 8; c++){
+                    if(levels[r][c] == e.getSource()){
+                        rr = r;
+                        cc = c;
+                        break;
+                    }
+                }
+            }
+            //TODO:
+            //open up level at levels[rr][cc]
+        }
+    }
 }
