6e9d9e9ed7bcb70e2b6ca140b861686fad150f90
Derek <derek.schuster@yahoo.com>
2015-03-02 18:58:43 -0500

    updated Application.java

--- Application.java
+++ Application.java
@@ -1,7 +1,8 @@
 public class Application{
     public static void main(String[] args){
       Frame frame = new Frame();
+      StartPanel sp = new StartPanel();
+      frame.add(sp);
       frame.setVisible(true);
-      
     }
 }
