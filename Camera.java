0adfe2253d2a18b2839b23715293d5d9da31b90f
pierfier <fabrispierre@gmail.com>
2015-03-02 15:18:34 -0500

    Created new Class Camera.java

---
+++ Camera.java
@@ -0,0 +1,8 @@
+
+//This class works the camera for the game
+//as the player moves around the camera follows it
+//The technique is just smoothly moving everything to the opposite direction of the character's motion
+
+public class Camera{
+    
+}
