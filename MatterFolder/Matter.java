e9fe785046a4f90c8f7be8f4bd9da6ec1d3ba6ca
Derek <derek.schuster@yahoo.com>
2015-03-03 17:19:03 -0500

    updated Matter.java

--- MatterFolder/Matter.java
+++ MatterFolder/Matter.java
@@ -1,16 +1,28 @@
 public abstract class Matter{
     
-    final int HEIGHT = 100;
-    final int WIDTH = 100;
-    int locX;
-    int locY;
+    private final int HEIGHT = 100;
+    private final int WIDTH = 100;
+    private int locX;
+    private int locY;
 
     public Matter(int locX, int locY){
         this.locX = locX;
         this.locY = locY;
     }
     
+    public void setX(int locX){
+        this.locX = locX;
+    }
     
+    public void setY(int locY){
+        this.locY = locY;
+    }
     
+    public int getX(){
+        return locX;
+    }
     
+    public int getY(){
+        return locY;   
+    }
 }
