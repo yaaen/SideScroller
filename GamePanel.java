8238c967930f35da0d061b422184e9f1ef0336ac
Derek <derek.schuster@yahoo.com>
2015-03-04 18:47:33 -0500

    updated GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -16,13 +16,13 @@ public class GamePanel{
         //Add collision detection, camera motion, and other movement calls here.
         //Or they can be put in another method that will separately update the figures, in another thread,
         //while this method is repeatedly called by itself with or without the changes from the other update method
-
-        player.paint(g);
         
         //draws everything onto the screen
         for(int i = 0; i < matter.length; i++){
             for(int j = 0; j < matter[0].length; j++){
-                matter[i][j].paint(g);
+               //we're gonna have to paint the square icon wherever and get its location,
+               //which should all be in the class so I think it'd be something like:
+               g.drawImage(matter[i][j].getImage(), matter[i][j].getX(), matter[i][j].getY(), null);
             }
         }
     }
