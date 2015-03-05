172fb87f1ae6078d618c7286eeb058c682fab0bb
Derek <derek.schuster@yahoo.com>
2015-03-04 20:32:27 -0500

    updated FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -1,5 +1,15 @@
 public class FileReader{
-  public FileReader(){
   
+  public FileReader(){
+    
+  }
+  
+  public void readLevelFromFile(int level){
+      //WILL NOT BE VOID LATER ON
+      //create a file object for the file given a specified level
+      //make an arraylist to contain all the positions
+      //convert the positions to a more readable form
+      //convert to an array
+      //return that
   }
 }
