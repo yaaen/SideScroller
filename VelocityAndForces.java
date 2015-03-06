9dc15d8c00ca25e0e2b673af50aec1a85e04e554
EthanSchoen <e.schoen@yahoo.com>
2015-03-06 13:05:36 -0500

    Update VelocityAndForces.java

--- VelocityAndForces.java
+++ VelocityAndForces.java
@@ -1,4 +1,16 @@
 public interface VelocityAndForces(){
-    final int GRAVITY = 3; //dunno what this should be yet
+    public static final int GRAVITY = 3; //dunno what this should be yet
     
+    public void setSpeed(int v);
+    public int getSpeed();
+    
+    //probably have 1 = right, 2 = up right, etc. going around with a total of 8 or more. Using angles is a possibility as well
+    public void setDirection(int d);
+    public int getDirection();
+    
+    public void setPosition(int x, int y);
+    //don't knwo how we will want to got about getting position yet
+    
+    public void setDrag(int d);
+    public int getDrag(); //might not need this
 }
