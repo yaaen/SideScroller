0405babae5bb399dab3bfc2bb6a0c5ec148c6bfe
EthanSchoen <e.schoen@yahoo.com>
2015-03-06 12:58:36 -0500

    Update ObjectDimensions.java

--- ObjectDimensions.java
+++ ObjectDimensions.java
@@ -1,9 +1,25 @@
 public class ObjectDimensions {
-  public void addObjectDimensions(int w, int i){
   
+  public ObjectDimensions(){
+    //might need to have the level loaded into this constructor
   }
   
-  public boolean collisionCheck(object1, object2){
-  
+  public boolean collisionCheck(Matter object1, Matter object2){
+    object1XLocation = object1.getX();
+    object1YLocation = object1.getY();
+    
+    object2XLocation = object2.getX();
+    object2YLocation = object2.getY();
+    
+    if(/*check for collsion*/){
+      
+      //true means collision detected (player get damage, wins level, or cant move)
+      
+      return true;
+    }
+    
+    //no collision found, default to false
+    
+    return false;
   }
 }
