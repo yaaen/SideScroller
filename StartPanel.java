02b219c19b358b4ba4fc268882a8c419ba6e01c9
Derek <derek.schuster@yahoo.com>
2015-03-07 17:45:50 -0500

    updated StartPanel.java

--- StartPanel.java
+++ StartPanel.java
@@ -1,46 +1,45 @@
-import java.awt.Graphics;
+
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
-public class StartPanel extends JPanel{
-  
-  int chosen = -1;
-  
-  public StartPanel(){
-     setSize(1600, 1000);
-        setLayout(null);
-        
+public class StartPanel extends JPanel {
+
+    JButton[][] levels = new JButton[3][8];
+    int chosen = -1;
+    chosenEvent e = new chosenEvent();
+
+    public StartPanel() {
+        setBorder(new EmptyBorder(40, 40, 40, 40));
+        setSize(1600, 1000);
+        setLayout(new GridLayout(0, 8, 40, 40));
+
         int count = 1;
         int yValue = 150;
         for(int r = 0; r < 3; r++){
             for(int c = 0; c < 8; c++){
-                levels[r][c] = new JButton(String.valueOf(count)); //Array levels is never defined
-                levels[r][c].setLocation(c*190 + 80, yValue);
-                levels[r][c].setSize(100, 100);
+                levels[r][c] = new JButton(String.valueOf(count));
+                levels[r][c].setMargin(new Insets(50, 50, 50, 50));
+                levels[r][c].addActionListener(e);
                 this.add(levels[r][c]);
                 count++;
             }
             yValue += 200;
         }
-  }
-  
-  @Override
-  public void paint(Graphics g){
-      //TODO:
-      //paint the background of the start screen
-  }
-  
-  public int chosen(){
-    return chosen;
-  }
-  
-  public class event implements ActionListener{
+    }
+    
+    public int chosen() {
+        return chosen;
+    }
+
+    public class chosenEvent implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            int rr = 0, cc = 0;
             for(int r = 0; r < 3; r++){
                 for(int c = 0; c < 8; c++){
                     if(levels[r][c] == e.getSource()){
