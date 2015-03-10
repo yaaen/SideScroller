6efc0b8de5280643259cd7a5847515fcf33256d1
Pierre Fabris <fabrispierre@gmail.com>
2015-03-10 12:10:09 -0400

    Fixed ObjectDimensions class Class

--- ObjectDimensions.java
+++ ObjectDimensions.java
@@ -6,19 +6,19 @@ public class ObjectDimensions {
   
   //should this be called by the player class so it uses it to know if it can move or not then?
   public boolean collisionCheck(Matter object1, Matter object2){
-    object1XLocation = object1.getX();
-    object1YLocation = object1.getY();
+    int object1XLocation = object1.getX();
+    int object1YLocation = object1.getY();
     
-    object2XLocation = object2.getX();
-    object2YLocation = object2.getY();
+    int object2XLocation = object2.getX();
+    int object2YLocation = object2.getY();
     
-    if(/*check for collsion*/){
+  /*  if(/*check for collsion*/){
       
       //true means collision detected (player get damage, wins level, or cant move)
       
       return true;
     }
-    
+    */
     //no collision found, default to false
     
     return false;
