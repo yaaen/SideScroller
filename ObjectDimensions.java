3613696e7d995101fedecd8be1903aa0ba2cf81c
Derek <derek.schuster@yahoo.com>
2015-03-13 21:50:30 -0400

    updated ObjectDimensions.java

--- ObjectDimensions.java
+++ ObjectDimensions.java
@@ -4,6 +4,7 @@ public class ObjectDimensions {
   
   public ObjectDimensions(){
     //might need to have the level loaded into this constructor
+    //or we could just add another method to have that information passed so it can be updated each level
   }
   
   //should this be called by the player class so it uses it to know if it can move or not then?
