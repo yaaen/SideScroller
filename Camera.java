efd45977fa450d0584ae12efb57ca7378785c4a2
Pierre Fabris <fabrispierre@gmail.com>
2015-03-14 13:43:55 -0400

    Simple camera movement is possible
    Changes in the future would have to be when the player is in a certain frame that it should allow the camera to move.

--- Camera.java
+++ Camera.java
@@ -4,16 +4,37 @@
 //The technique is just smoothly moving everything to the opposite direction of the character's motion
 
 public class Camera{
+    /*enum for the different directions that the camera can move
+    it is tricky because the camera movement is relative to block movement.
+    For example, to move the camera upwards, all of the blocks need to be moved downward
+    */
     
-    //found door or not which would lock the camera in place
+    //all of the matter pieces currently in the frame
+    //we should discuss if new pieces should be introduced in this class or the gamePanel class
+    Block[][] field;
+    
+    Player player;
+    
+    //these save how much the player has moved
+    int dx, dy;
+    
+    //door in frame which would lock the camera in place
     boolean lock = false;
-
-    public Camera(){
-        
+    
+    public Camera(Block[][] field, Player player){
+        this.field = field;
+        this.player = player;
     }
     
     public void move(){
-        
+        if(!lock){
+            for(int i = 0; i < field.length; i++){
+            for(int j = 0; j < field[0].length; j++){
+                field[i][j].locY -= dy;
+                field[i][j].locX -= dx;
+            }
+        }
+        }
     }
 }
 
