00ac33f2a4c3a9955aa95e569cfcf8a82cf7136e
Derek <derek.schuster@yahoo.com>
2015-03-14 12:01:39 -0400

    updated Frame.java

--- Frame.java
+++ Frame.java
@@ -2,6 +2,10 @@ import javax.swing.JFrame;
 
 public class Frame extends JFrame{
   public Frame(){
+    //what is this setBounds and what are the
+    //100 and 25 there for? shouldn't it be
+    //set size(1600, 1000)?
+    //also what layout should we go for here?
     setBounds(100,25,1600,1000);
     setTitle("Side Scroller");
     setResizable(false);
