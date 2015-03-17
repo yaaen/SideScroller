c80c381d62bf607446bc1024a573407fb5e77ccb
EthanSchoen <e.schoen@yahoo.com>
2015-03-16 20:27:21 -0400

    Update Block.java

--- MatterFolder/Block.java
+++ MatterFolder/Block.java
@@ -1,12 +1,29 @@
 package MatterFolder;
 
-public class Block extends Matter{
-  
-  public Block(){
+import java.awt.*;
+
+public class Block extends Matter {
+	
+	Color bColor;
+	
+	public Block() {
 		
-  }
-  
-  public Block(int locX, int locY){
-    super(locX, locY);
-  }
+	}
+	
+	public Block(int locX, int locY) {
+		super(locX, locY);
+	}
+	
+	public Block(int locX, int locY, Color c) {
+		super(locX, locY);
+		bColor = c;
+	}
+	
+	public Color getColor() {
+		return bColor;
+	}
+	
+	public void setColor(Color c) {
+		bColor = c;
+	}
 }
