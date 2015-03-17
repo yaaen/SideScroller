a188ea27ca9de131c08bd0adfa405f9af55b0b81
Derek <derek.schuster@yahoo.com>
2015-03-17 18:51:31 -0400

    updated Player.java

--- MatterFolder/Player.java
+++ MatterFolder/Player.java
@@ -4,4 +4,20 @@ public class Player extends Character{
     public Player(int locX, int locY){
         super(locX, locY);
     }
+    
+    public void moveRight(){
+        locX++;    
+    }
+    
+    public void moveRight(int toMove){
+        locX += toMove;
+    }
+    
+    public void moveLeft(){
+        locX--;
+    }
+    
+    public void moveLeft(int toMove){
+        locX -= toMove;
+    }
 }
