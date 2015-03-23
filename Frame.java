7dd0e7b3a6b3e1af863e98eb1532c5f46e653242
Derek <derek.schuster@yahoo.com>
2015-03-23 19:30:58 -0400

    updated Frame.java

--- Frame.java
+++ Frame.java
@@ -6,5 +6,9 @@ public class Frame extends JFrame{
     setTitle("Side Scroller");
     setResizable(false);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //do you think we should sat a layout here?
+    //even though were only adding the panel should
+    //we put something or will it be fine without one?
+    //or even just good programming practice to have one?
   }
 }
