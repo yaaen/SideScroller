acb7cf40f03d37e370739928136ba611af39e9ff
EthanSchoen <e.schoen@yahoo.com>
2015-03-24 18:34:59 -0400

    Update Player.java
    implementing changes made in Matter

--- MatterFolder/Player.java
+++ MatterFolder/Player.java
@@ -8,34 +8,34 @@ public class Player extends Character{
     //pr0ves
     //locX and locY are private so I made the method modLocX() and modLocY() in Matter.java
     public void moveRight(){
-        modLocX(1,true);
+        modLocX(1);
     }
     
     public void moveRight(int toMove){
-        modLocX(toMove,true);
+        modLocX(toMove);
     }
     
     public void moveLeft(){
-        modLocX(1,false);
+        modLocX(1);
     }
     
     public void moveLeft(int toMove){
-        modLocX(toMove,false);
+        modLocX(toMove);
     }
     
     public void moveUp(){
-        modLocY(1,true);
+        modLocY(1);
     }
     
     public void moveUp(int toMove){
-        modLocY(toMove,true);
+        modLocY(toMove);
     }
     
     public void moveDown(){
-        modLocY(1,false);
+        modLocY(1);
     }
     
     public void moveDown(int toMove){
-        modLocY(toMove,false);
+        modLocY(toMove);
     }
 }
