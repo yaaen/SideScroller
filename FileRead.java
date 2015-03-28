440c510b0a27c841b360c9c0a6db725699c3ec7d
Derek <derek.schuster@yahoo.com>
2015-03-28 18:56:57 -0400

    updated FileRead.java
    added documentation

--- FileRead.java
+++ FileRead.java
@@ -5,33 +5,35 @@ import java.io.FileReader;
 
 public class FileRead {
 
-    private String[][] levelArray;
+    	private String[][] levelArray;
 
-	//we might want to just use a constructor with no parameters because we never know what level they will open first
-	//so the code might be cleaner to just always have to update it the same way instead of the first time through
-	//the constructor.
+	/**
+	 * This constructor takes no parameters and simply creates the
+	 * FileRead class. setLevelArray() must be called after this to
+	 * setup the levelArray
+	 */ 
 	public FileRead(){
 		
 	}
 
-	//shouldn't need any of the following anymore but left it just incase my edits cause an error
-    //pr0ves
-    //made this constructor so that everytime you create a FileRead object it creates automatically the level array
-    //public FileRead(int level) {
-      // this.levelArray = readLevelFromFile(level);
-    //}
-
+	/**
+	 * Takes in an int, level, and from this sets levelArray using 
+	 * the data read in from the text file
+	 * 
+	 * @param level	the level from which to set the Array 
+	 */
 	public void setLevelArray(int level){
 		this.levelArray = readLevelFromFile(level);
 	}
 
-
-    //pr0ves
-    //i think you could make this method in a better way, for example you can jump the arraylist part and convert directly levelString to levelArray    
-    //also you could delete the "n" in the level file, because every line is terminated by "\n" by default
-
+	/**
+	 * Takes a given level and reads from its corresponding text file and puts 
+	 * the letters into a string array to be parsed later on by another method
+	 * 
+	 * @param level	the level that is to be read and placed into the String array
+	 * @return	String[][] that contains the level specified
+	 */
 	private String[][] readLevelFromFile(int level) {
-
 		//create a file to read in the level and add to a string
 		File file = new File("Levels/level" + level + ".txt"); //this might need ".txt" cant try out right now
 		String levelString = "";
@@ -52,22 +54,27 @@ public class FileRead {
 			System.out.print(e.toString());
 		}
 
-        //split levelString to an array that contains every line
-        String[] array = levelString.split("\n");
-        int length = array[0].length();
-        int height = array.length;
-        String[][] levelArray = new String[height][length];
+        	//split levelString to an array that contains every line
+        	String[] array = levelString.split("\n");
+        	int length = array[0].length();
+        	int height = array.length;
+        	String[][] levelArray = new String[height][length];
 
-        //split every line by every characters
+        	//split every line by every characters
 		for (int i = 0; i < height; i++) {
-				levelArray[i] = array[i].split("");
-
+			levelArray[i] = array[i].split("");
 		}
 
 		return levelArray;
 	}
 
-    public String[][] getLevelArray() {
-        return this.levelArray;
-    }
+	/**
+	 * This method returns a String[][] that contains the letters from the
+	 * text file that was last read in
+	 * 
+	 * @return	String[][] containing the most recently read in level
+	 */
+    	public String[][] getLevelArray() {
+        	return this.levelArray;
+    	}
 }
