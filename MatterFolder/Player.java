5d551b91eaa20839afe2af664d645216bcfff4bb
Derek <derek.schuster@yahoo.com>
2015-04-03 08:22:50 -0400

    updated Player.java

--- MatterFolder/Player.java
+++ MatterFolder/Player.java
@@ -1,15 +1,22 @@
 package MatterFolder;
 
-public class Player extends Character{
-    public Player(int locX, int locY){
+import java.awt.Color;
+
+public class Player extends Character {
+
+    public Player(int locX, int locY) {
         super(locX, locY);
     }
+    
+    public Player(int locX, int locY, Color c){
+        super(locX, locY, c);
+    }
 
-    public void moveLateral(int dist){
-		modLocX(dist);
-	}
-	
-	public void moveVertical(int dist){
-		modLocY(dist);
-	}
+    public void moveLateral(int dist) {
+        modLocX(dist);
+    }
+
+    public void moveVertical(int dist) {
+        modLocY(dist);
+    }
 }
