26f92d5e1657e3fcc5628db9edbca2d2089240d4
Derek <derek.schuster@yahoo.com>
2015-04-03 08:21:31 -0400

    updated Character.java

--- MatterFolder/Character.java
+++ MatterFolder/Character.java
@@ -1,7 +1,14 @@
 package MatterFolder;
 
-public class Character extends Matter{
-       public Character(int locX, int locY){
-              super(locX, locY);
-       }
+import java.awt.Color;
+
+public class Character extends Matter {
+
+    public Character(int locX, int locY) {
+        super(locX, locY);
+    }
+
+    public Character(int locX, int locY, Color c) {
+        super(locX, locY, c);
+    }
 }
