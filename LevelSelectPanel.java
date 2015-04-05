edd66e6c1db44bb162bf2a36cc6835029555b473
Derek <derek.schuster@yahoo.com>
2015-04-05 11:47:15 -0400

    updated LevelSelectPanel.java
    changed it to a different layout manager to have more control and added some more buttons for implementation later on

--- LevelSelectPanel.java
+++ LevelSelectPanel.java
@@ -1,7 +1,9 @@
-import java.awt.Graphics;
+
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,58 +11,101 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class LevelSelectPanel extends JPanel {
 
     int rows = 3;
     int cols = 8;
     JButton[][] levels = new JButton[rows][cols];
-    private int chosen = -1;
-    ChosenEvent e = new ChosenEvent();
+    JButton credits;
+    JButton start;
+    JButton settings;
+    ExtrasEvent ee = new ExtrasEvent();
+    int chosen = -1;
+    ChosenEvent ce = new ChosenEvent();
 
-    public LevelSelectPanel() {
-        setBorder(new EmptyBorder(40, 40, 40, 40));
+    public LevelSelectPanel(int thign) {
         setSize(1600, 1000);
-        setLayout(new GridLayout(4, 0, 40, 40));
+
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        setLayout(gridbag);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(25, 25, 25, 25);
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        int gridx = 0;
+        int gridy = 0;
 
         //number of files in the Levels folder
         String dirString = "/Levels";
-        Path dir = Paths.get(dirString); // ist this supposed to be paths (plural?)
+        Path dir = Paths.get(dirString);
         int numOfLevels = dir.getNameCount() + 1;
 
         int levelNum = 1;
         for(int r = 0; r < rows; r++){
             for(int c = 0; c < cols; c++){
                 levels[r][c] = new JButton(String.valueOf(levelNum));
-                levels[r][c].setFont(new Font("Arial", Font.Plain, 40));
-                levels[r][c].setMargin(new Insets(50, 50, 50, 50));
-                levels[r][c].addActionListener(e);
+                levels[r][c].setFont(new Font("Arial", Font.PLAIN, 40));
+                levels[r][c].addActionListener(ce);
                 levels[r][c].setBackground(Color.MAGENTA);
+                gbc.gridx = gridx;
+                gbc.gridy = gridy;
                 if(levelNum <= numOfLevels){
                     levels[r][c].setEnabled(true);
                 } else{
                     levels[r][c].setEnabled(false);
                 }
-                this.add(levels[r][c]);
+                this.add(levels[r][c], gbc);
                 levelNum++;
+                gridx++;
             }
+            gridx = 0;
+            gridy++;
         }
+
+        credits = new JButton("Credits");
+        credits.setFont(new Font("Arial", Font.PLAIN, 40));
+        credits.setBackground(Color.MAGENTA);
+        credits.addActionListener(ee);
+        gbc.gridx = 0;
+        gbc.gridy = gridy;
+        gbc.gridwidth = 2;
+        this.add(credits, gbc);
+
+        start = new JButton("Menu");
+        start.setFont(new Font("Arial", Font.PLAIN, 40));
+        start.setBackground(Color.MAGENTA);
+        start.addActionListener(ee);
+        gbc.gridx = 3;
+        gbc.gridy = gridy;
+        gbc.gridwidth = 2;
+        this.add(start, gbc);
+
+        settings = new JButton("Settings");
+        settings.setFont(new Font("Arial", Font.PLAIN, 40));
+        settings.setBackground(Color.MAGENTA);
+        settings.addActionListener(ee);
+        gbc.gridx = 6;
+        gbc.gridy = gridy;
+        gbc.gridwidth = 2;
+        this.add(settings, gbc);
     }
-    
-    public void paintComponent(Graphics g){
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         g.setColor(Color.CYAN);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        
+
         g.setColor(Color.BLUE);
         g.fillRect(0, 0, 470, 320);
         g.setColor(Color.RED);
         g.fillRect(100, 700, 200, 190);
         g.setColor(Color.GREEN);
         g.fillRect(1200, 100, 300, 700);
-        
+
         g.setColor(Color.BLUE);
         g.fillRect(1050, 600, 270, 320);
         g.setColor(Color.RED);
@@ -68,11 +113,11 @@ public class LevelSelectPanel extends JPanel {
         g.setColor(Color.GREEN);
         g.fillRect(250, 600, 300, 200);
     }
-    
+
     public void setChosen(int chosen) {
         this.chosen = chosen;
     }
-    
+
     public int getChosen() {
         return chosen;
     }
@@ -89,6 +134,17 @@ public class LevelSelectPanel extends JPanel {
                         break loop;
                     }
                 }
+            }
+        }
+    }
+
+    public class ExtrasEvent implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(credits == e.getSource()){
+            } else if(start == e.getSource()){
+            } else if(settings == e.getSource()){
             }
         }
     }
