c7b65d795f91207d2b87f9be3724ce6f64c89394
Derek <derek.schuster@yahoo.com>
2015-04-18 17:12:49 -0400

    updated ObjectDimensions.java

--- src/tools/ObjectDimensions.java
+++ src/tools/ObjectDimensions.java
@@ -6,11 +6,8 @@ public class ObjectDimensions {
 
 	private int blockSize;
 
-	public ObjectDimensions(int size) {
-		//might need to have the level loaded into this constructor
-		//or we could just add another method to have that information passed so it can be updated each level
-		//  we might not need it if we just use the get location stuff in collisionCheck()
-		blockSize = size;
+	public ObjectDimensions(int blockSize) {
+		this.blockSize = blockSize;
 	}
 
 	//should this be called by the player class so it uses it to know if it can move or not then?
@@ -20,9 +17,7 @@ public class ObjectDimensions {
 
 		int object2XLocation = object2.getX();
 		int object2YLocation = object2.getY();
-
-		//System.out.println("ob1x:" + object1XLocation + "  ob1y:" + object1YLocation + "   ob2x:" + object2XLocation + "  ob2y:" + object2YLocation);
-
+		
 		//no collision found, default to false
 		//true means collision detected (player get damage, wins level, or cant move)
 		return ((object1XLocation <= (object2XLocation - blockSize) || object1XLocation >= object2XLocation + blockSize)
