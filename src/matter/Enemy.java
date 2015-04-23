5e60c8c3ff6191fe731dc451f8f31c8f73886067
Derek <derek.schuster@yahoo.com>
2015-04-23 18:47:42 -0400

    updated Enemy.java

--- src/matter/Enemy.java
+++ src/matter/Enemy.java
@@ -1,7 +1,18 @@
 package matter;
 
 public class Enemy extends Character{
+  
+  private boolean kills;
+  
   public Enemy(int locX, int locY){
     super(locX, locY);
+  }
+  
+  private void setKills(boolean kills){
+    this.kills = kills;
+  }
+  
+  private boolean killsPlayer(){
+    return kills;
   }
 }
